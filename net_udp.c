1a546144bf45e1b364ae594b4065ef80dbae3ada
atsb <44937323+atsb@users.noreply.github.com>
2023-09-13 19:22:46 +0200

    Forgot one file - this removes the old fashioned extern

--- net_udp.c
+++ net_udp.c
@@ -50,7 +50,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <arpa/inet.h>
 #endif /* __APPLE__ || MACOSX */
 
-extern int gethostname (char *, int);
 extern int close (int);
 
 extern cvar_t hostname;
