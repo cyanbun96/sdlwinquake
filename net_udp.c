abd903741ab2132660aa46208ca63c15c2ee05a8
Cyan <cyanbun96@gmail.com>
2024-11-10 15:13:27 +0200

    included unistd.h in net_udp.c so it finds gethosename properly

--- net_udp.c
+++ net_udp.c
@@ -29,6 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <netdb.h>
 #include <sys/param.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 #endif
 
 #ifdef _WIN32
